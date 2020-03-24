4eb23ad5a43708c0647e3a71b32c66251980105d
wangshengzhe <15088665674@163.com>
2020-03-24 12:04:19 +0800

    第一次用idea向本地仓库提交一个java工程

--- git-test-porgect/src/com/itheima/git/FirstClass.java
+++ git-test-porgect/src/com/itheima/git/FirstClass.java
@@ -4,5 +4,6 @@ package com.itheima.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello github i from idea1111");
+        System.out.println("2222222");
     }
 }
